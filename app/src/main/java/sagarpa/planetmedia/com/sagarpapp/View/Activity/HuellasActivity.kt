42e8b64b83f4c12ac88e839e148e80e857dda13e
sergio ivan lopez monzon <sergioivan154@gmail.com>
2018-05-09 11:12:37 -0500

    otro gradle menu

---
+++ app/src/main/java/sagarpa/planetmedia/com/sagarpapp/View/Activity/HuellasActivity.kt
@@ -0,0 +1,13 @@
+package sagarpa.planetmedia.com.sagarpapp.View.Activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import sagarpa.planetmedia.com.sagarpapp.R
+
+class HuellasActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_huellas)
+    }
+}
--- app/src/main/java/sagarpa/planetmedia/com/sagarpapp/View/Activity/LoginActivity.kt
+++ app/src/main/java/sagarpa/planetmedia/com/sagarpapp/View/Activity/LoginActivity.kt
@@ -4,10 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import sagarpa.planetmedia.com.sagarpapp.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
