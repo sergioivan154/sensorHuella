fc42eaab237ca7aab5b1de31c61c02162ce49331
sergio ivan lopez monzon <sergioivan154@gmail.com>
2018-05-09 11:14:46 -0500

    menu

--- app/src/main/java/sagarpa/planetmedia/com/sagarpapp/View/Activity/HomeActivity.kt
+++ app/src/main/java/sagarpa/planetmedia/com/sagarpapp/View/Activity/HomeActivity.kt
@@ -8,7 +8,10 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 import sagarpa.planetmedia.com.sagarpapp.R
+
 
 class HomeActivity: AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
